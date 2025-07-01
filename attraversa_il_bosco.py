ab470af3dddcf0d3eebf92a009554e11c14aa4f2
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-07-01 18:58:36 +0200

    Update attraversa_il_bosco.py

--- attraversa_il_bosco.py
+++ attraversa_il_bosco.py
@@ -10,11 +10,11 @@ comando="INIZIO"
 def instruzioni():
     print('ECCO I COMANDI\n')
     print('MAPPA --> visvualizza la mappa\n')
-    print('NORD --> vai a nord\SUD vai a sud\nEST vai a est,\nOVEST vai a ovest\n')
+    print('NORD --> vai a nord\nSUD vai a sud\nEST vai a est,\nOVEST vai a ovest\n')
     print('ASCIA --> abbatte un cespuglio\n')
     print('END --> interrompo il gioco')
 
-posizione=[1,0] #la posizione è una lista dove i valori sono le cordinate nella matrice
+posizione=[4,0] #la posizione è una lista dove i valori sono le cordinate nella matrice
 inizio=[4,3]
 fine=[0,3] #stessa cosa per inizio e fine
 
@@ -25,4 +25,11 @@ while comando!= "END":
     elif comando=='MAPPA':
         for r in range(len(mappa)):
              print(mappa[r])
-
+    if comando=='NORD':
+        print('prima ',posizione)
+        if posizione[0]-1>=0:
+            posizione[0]=posizione[0]-1
+        else:
+            print('SEI SUL CONFINE')
+        print('DOPO ',posizione) #COSì FUNZIA MA NON MI PIACE. DOMANI SCRIVO LA FUNZIONE E USO VAR COMODO POSIZIONE PRECEDENTE
+    
