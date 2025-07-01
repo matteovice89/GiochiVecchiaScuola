4dfc88fdd554e7d1f59b22dc32fb0d880ab0836e
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-07-01 22:50:45 +0200

    Update attraversa_il_bosco.py

--- attraversa_il_bosco.py
+++ attraversa_il_bosco.py
@@ -1,11 +1,12 @@
-mappa=[
-    ["*","S","S","U","A"],
-    ["A","C","A","A","A"],
-    ["A","S","S","S","A"],
-    ["S","S","A","S","A"],
-    ["A","A","S","I","A"]
-    ]
-comando="INIZIO"
+mappa = [
+    ["*", "S", "S", "U", "A"],
+    ["A", "C", "A", "A", "A"],
+    ["A", "S", "S", "S", "A"],
+    ["S", "S", "A", "S", "A"],
+    ["A", "A", "S", "I", "A"]
+]
+comando = "INIZIO"
+
 
 def instruzioni():
     print('ECCO I COMANDI\n')
@@ -14,22 +15,37 @@ def instruzioni():
     print('ASCIA --> abbatte un cespuglio\n')
     print('END --> interrompo il gioco')
 
-posizione=[4,0] #la posizione è una lista dove i valori sono le cordinate nella matrice
-inizio=[4,3]
-fine=[0,3] #stessa cosa per inizio e fine
 
-while comando!= "END":
-    comando=str(input('INSERISCI UN COMANDO (SCRIVI ISTRUZIONI PER SAPERE COSA FARE)\n'))
+def confini(posizione):
+    if posizione[0] < 0 or posizione[0] > 4 or posizione[1] < 0 or posizione[1] > 4:
+        print('SEI SUL CONFINE')
+        controllo=0
+        return (controllo)
+
+
+posizione = [4, 3]  # la posizione è una lista dove i valori sono le cordinate nella matrice
+posizionev = posizione  # varibile di comodo per gestire i confini
+inizio = [4, 3]
+fine = [0, 3]  # stessa cosa per inizio e fine
+
+while comando != "END":
+    comando = str(input('INSERISCI UN COMANDO (SCRIVI ISTRUZIONI PER SAPERE COSA FARE)\n'))
     if comando == 'ISTRUZIONI':
         instruzioni()
-    elif comando=='MAPPA':
+    elif comando == 'MAPPA':
         for r in range(len(mappa)):
-             print(mappa[r])
-    if comando=='NORD':
-        print('prima ',posizione)
-        if posizione[0]-1>=0:
-            posizione[0]=posizione[0]-1
+            print(mappa[r])
+    if comando == 'NORD':
+        posizione[0] = posizione[0] - 1
+        ascisse = posizione[0]
+        ordinate = posizione[1]
+
+        if confini(posizione) != 0:
+            mappa[ascisse][ordinate] = 'P'
+            posizionev = posizione
+            for r in range(len(mappa)):
+                print(mappa[r])
         else:
-            print('SEI SUL CONFINE')
-        print('DOPO ',posizione) #COSì FUNZIA MA NON MI PIACE. DOMANI SCRIVO LA FUNZIONE E USO VAR COMODO POSIZIONE PRECEDENTE
+            posizione = posizionev
+
     
