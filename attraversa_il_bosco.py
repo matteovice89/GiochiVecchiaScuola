a4aa5aeb31a85ee812c60752406cd8c9d4e7d838
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-07-01 17:56:11 +0200

    Update attraversa_il_bosco.py

--- attraversa_il_bosco.py
+++ attraversa_il_bosco.py
@@ -13,7 +13,16 @@ def instruzioni():
     print('NORD --> vai a nord\SUD vai a sud\nEST vai a est,\nOVEST vai a ovest\n')
     print('ASCIA --> abbatte un cespuglio\n')
     print('END --> interrompo il gioco')
+
+def confini(posizone):
+    if posizione[0]>4 or posizione[0]<0:
+        print('SEI SUL CONFINE NON PUOI PROCEDERE')
+        
     
+    
+posizione=[1,0] #la posizione è una lista dove i valori sono le cordinate nella matrice
+inizio=[4,3]
+fine=[0,3] #stessa cosa per inizio e fine
 
 while comando!= "END":
     comando=str(input('INSERISCI UN COMANDO (SCRIVI ISTRUZIONI PER SAPERE COSA FARE)\n'))
@@ -22,3 +31,8 @@ while comando!= "END":
     elif comando=='MAPPA':
         for r in range(len(mappa)):
              print(mappa[r])
+    elif comando == 'NORD':
+        posizione[0]=posizione[0]-1
+        if confini(posizione)==True:
+           posizione[0]=posizione[0]+1  
+    print(posizione)
